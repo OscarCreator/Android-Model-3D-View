394f4176666c4272681c0453039015fc5a0ac925
OscarCreator <oscar.creator13@gmail.com>
2021-12-07 13:36:44 +0100

    Adding projection matrix and camera. Also motion to triangle

--- app/src/main/java/com/oscarcreator/android3dmodelview/BasicRenderer.kt
+++ app/src/main/java/com/oscarcreator/android3dmodelview/BasicRenderer.kt
@@ -2,6 +2,8 @@ package com.oscarcreator.android3dmodelview
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
+import android.os.SystemClock
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
@@ -11,6 +13,15 @@ import javax.microedition.khronos.opengles.GL10
 class BasicRenderer() : GLSurfaceView.Renderer {
 
     private lateinit var triangle: Triangle
+
+    private val modelViewProjectionMatrix = FloatArray(16)
+    private val projectionMatrix = FloatArray(16)
+    private val viewMatrix = FloatArray(16)
+    private val rotationMatrix = FloatArray(16)
+
+    // renderer code runs on a separate thread
+    @Volatile
+    var angle = 0f
 
     override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
         // set the background frame color
@@ -23,12 +34,36 @@ class BasicRenderer() : GLSurfaceView.Renderer {
     override fun onSurfaceChanged(unused: GL10?, width: Int, height: Int) {
         // Set opengl viewport to fill entire surface
         GLES20.glViewport(0,0, width, height)
+
+        val ratio: Float = width.toFloat() / height.toFloat()
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
     }
 
     override fun onDrawFrame(p0: GL10?) {
         // redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-        triangle.draw()
+
+        // set the camera position: View matrix
+        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, 3f, 0f, 0f, 0f, 0f, 1f, 0f)
+
+        // calculate the projection and view transformation
+        Matrix.multiplyMM(modelViewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+
+        val scratch = FloatArray(16)
+
+        // create a rotation transformation for the triangle
+        //val time = SystemClock.uptimeMillis() % 4000L
+        //val angle = 0.090f * time.toInt()
+        Matrix.setRotateM(rotationMatrix, 0, angle, 0f, 0f, -1f)
+
+        // combine the rotation matrix with projection and camera view
+        Matrix.multiplyMM(scratch, 0, modelViewProjectionMatrix, 0, rotationMatrix, 0)
+
+        // draw shape
+        triangle.draw(scratch)
     }
 }
 
@@ -44,9 +79,9 @@ fun loadShader(type: Int, shaderCode: String): Int {
 
 const val COORDS_PER_VERTEX = 3
 var triangleCoords = floatArrayOf(
-    0f, 1f, 0f,  // top
-    -1f, -0.50f, 0f, // left
-    1f, 0f, 0f   // right
+    0f, 0.622f, 0f,  // top
+    -0.5f, -0.311f, 0f, // left
+    0.5f, -0.311f, 0f   // right
 )
 
 class Triangle {
@@ -71,16 +106,18 @@ class Triangle {
 
     private var program: Int
 
-    private var positionHandle: Int = 0
-    private var colorHandle: Int = 0
+    private var vPositionHandle: Int = 0
+    private var vColorHandle: Int = 0
+    private var vPMatrixHandle: Int = 0
 
     private val vertexCount: Int = triangleCoords.size / COORDS_PER_VERTEX
     private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
 
-    private val vertextShaderCode = """
+    private val vertexShaderCode = """
+        uniform mat4 uMVPMatrix;
         attribute vec4 vPosition;
         void main() {
-            gl_Position = vPosition;
+            gl_Position = uMVPMatrix * vPosition;
         }
     """.trimIndent()
 
@@ -94,7 +131,7 @@ class Triangle {
 
     init {
 
-        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertextShaderCode)
+        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
         val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
         program = GLES20.glCreateProgram().also {
             // add vertex shader to program
@@ -106,16 +143,16 @@ class Triangle {
         }
     }
 
-    fun draw() {
+    fun draw(modelViewProjectionMatrix: FloatArray) {
         GLES20.glUseProgram(program)
 
-        positionHandle = GLES20.glGetAttribLocation(program, "vPosition").also {
+        vPositionHandle = GLES20.glGetAttribLocation(program, "vPosition").also { positionHandle ->
             // enable a handle to the triangle vertices
-            GLES20.glEnableVertexAttribArray(it)
+            GLES20.glEnableVertexAttribArray(positionHandle)
 
             // prepare the triangle coordinate data
             GLES20.glVertexAttribPointer(
-                it,
+                positionHandle,
                 COORDS_PER_VERTEX,
                 GLES20.GL_FLOAT,
                 false,
@@ -124,17 +161,23 @@ class Triangle {
             )
 
             // get handle to fragment shader's color memeber
-            colorHandle = GLES20.glGetUniformLocation(program, "vColor").also { colorHandle
+            vColorHandle = GLES20.glGetUniformLocation(program, "vColor").also { vColorHandle ->
                 // set color for drawing triangle
-                GLES20.glUniform4fv(colorHandle, 1, color, 0)
+                GLES20.glUniform4fv(vColorHandle, 1, color, 0)
 
             }
+
+            // get handle to shape's transformation matrix
+            vPMatrixHandle = GLES20.glGetUniformLocation(program, "uMVPMatrix")
+
+            // pass the projection and view transformation to the shader
+            GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, modelViewProjectionMatrix, 0)
 
             // draw the triangle
             GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
 
             // disable vertex array
-            GLES20.glDisableVertexAttribArray(it)
+            GLES20.glDisableVertexAttribArray(positionHandle)
 
 
         }
--- app/src/main/java/com/oscarcreator/android3dmodelview/Model3DView.kt
+++ app/src/main/java/com/oscarcreator/android3dmodelview/Model3DView.kt
@@ -1,11 +1,20 @@
 package com.oscarcreator.android3dmodelview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.opengl.GLSurfaceView
+import android.view.MotionEvent
 
 class Model3DView(context: Context): GLSurfaceView(context) {
 
     private val renderer: BasicRenderer
+
+    companion object {
+        private const val TOUCH_SCALE_FACTOR: Float = 180f / 360f
+    }
+
+    private var previousX: Float = 0f
+    private var previousY: Float = 0f
 
     init {
         setEGLContextClientVersion(3)
@@ -17,4 +26,37 @@ class Model3DView(context: Context): GLSurfaceView(context) {
 
         renderMode = RENDERMODE_WHEN_DIRTY
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        val x: Float = event.x
+        val y: Float = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_MOVE -> {
+                var dx = x - previousX
+                var dy = y - previousY
+
+                // reverse direction
+                if (y > height / 2) {
+                    dx *= -1
+                }
+
+                if (x < width / 2) {
+                    dy *= -1
+                }
+
+                renderer.angle += (dx + dy) * TOUCH_SCALE_FACTOR
+                // tells the renderer that it's time to render next frame
+                requestRender()
+            }
+        }
+
+        previousX = x
+        previousY = y
+        return true
+    }
+
+
 }
